2500fe3a7521fc97c88eec79b158e7a3459cff40
Nicholas Stevens <74801713+NicholasStevens@users.noreply.github.com>
2022-12-20 09:30:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 15;
+let passwordOneEl = document.getElementById("passwordOne-el");
+let passwordTwoEl = document.getElementById("passwordTwo-el");
+function getRandomCharacter() {
+  let randomChar = Math.floor(Math.random() * characters.length + 1);
+  return characters[randomChar];
+}
+
+function generatePassword() {
+  let password1 = " ";
+  let password2 = " ";
+  for (let i = 0; i < passwordLength; i++) {
+    password1 += getRandomCharacter();
+    password2 += getRandomCharacter();
+  }
+  passwordOneEl.textContent = password1;
+  passwordTwoEl.textContent = password2;
+}
